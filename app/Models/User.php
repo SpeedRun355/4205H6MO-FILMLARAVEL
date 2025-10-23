85ce85d1a7f9c2b68031e2c9e8a1df83ee995794
Félix-Etienne Hamel <fe.hamel06@gmail.com>
2025-10-23 12:40:39 -0400

    ajout AUTH

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -20,9 +20,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'surname',
+        'email',
         'birthday',
         'username',
         'password',
+        'role',
     ];
 
     public function Film_User(){
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,8 @@ use App\Models\FilmUser;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/apropos', function () {
-    return view('films.apropos');
-})->name('apropos'); 
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route:: get ('/', [FilmController::class, 'index']);
 Route::post('/autocomplete', [FilmUserController::class,'autocomplete'])->name('autocomplete');
@@ -28,9 +27,30 @@ Route::resources([
                  'film'=> FilmController::class,
                  'filmUser'=> FilmUserController::class,
                 ]);
+
 Route::get('lang/{locale}', 
 [App\Http\Controllers\LocalizationController::class, 'index']);
 
 Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/profile', function () {
+        return 'User Profile';
+    });
+});
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin', function () {
+        return 'Admin Dashboard';
+    });
+    Route::get('/apropos', function () {
+        return view('films.apropos');
+    })->name('apropos');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', function () {
+        return 'User Profile';
+    });
+});
